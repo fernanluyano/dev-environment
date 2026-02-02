bbbc7fae11377e63bf9a0a0b2c00152aec9b9520
Fernando <fernando.berlanga1@gmail.com>
2026-02-02 09:14:36 -0500

    disabled copilot accpet Tab issue

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -7,13 +7,15 @@ return {
       -- Enable Copilot
       vim.g.copilot_enabled = false
 
-      -- Accept suggestion with Tab
-      vim.keymap.set("i", "<Tab>", 'copilot#Accept("\\<CR>")', {
+      -- Disable Copilot's default Tab mapping
+      vim.g.copilot_no_tab_map = true
+
+      -- Accept suggestion with Ctrl-Y (common convention)
+      vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
         expr = true,
         replace_keycodes = false,
         silent = true,
       })
-      vim.g.copilot_no_tab_map = true
 
       -- Additional keybindings
       vim.keymap.set("i", "<C-]>", "<Plug>(copilot-next)", { silent = true })
