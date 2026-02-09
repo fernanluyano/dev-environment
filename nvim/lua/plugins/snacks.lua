ba7809324a540b934045190e34893a1a7a57b565
Fernando <fernando.berlanga1@gmail.com>
2026-02-09 10:23:41 -0500

    file explorer floating

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -53,7 +53,27 @@ return {
     picker = {
       enabled = true,
       sources = {
-        explorer = { hidden = true },
+        explorer = {
+          auto_close = true,
+          hidden = true,
+          layout = {
+            { preview = true },
+            layout = {
+              box = "horizontal",
+              width = 0.8,
+              height = 0.8,
+              {
+                box = "vertical",
+                border = "rounded",
+                title = "{source} {live} {flags}",
+                title_pos = "center",
+                { win = "input", height = 1, border = "bottom" },
+                { win = "list", border = "none" },
+              },
+              { win = "preview", border = "rounded", width = 0.7, title = "{preview}" },
+            },
+          },
+        },
       },
     },
     notifier = { enabled = true },
