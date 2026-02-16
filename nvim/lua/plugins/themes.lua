356060467533ae2eca2a5c29fd6a82b07a303258
Fernando <fernando.berlanga1@gmail.com>
2026-02-15 19:49:27 -0500

    added neon yazi

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -4,7 +4,7 @@ return {
     -- dir = "/Users/fberlanga/dev/github/xcodedark.nvim",
     lazy = false,
     priority = 1000,
-    enabled = true,
+    enabled = false,
     config = function()
       require("xcodedark").setup({
         -- New color scheme with your specifications
@@ -60,6 +60,21 @@ return {
     end,
   },
   {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    enabled = true,
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+        colors = {
+          red = "#A0354A",
+        },
+      })
+      require("onedark").load()
+    end,
+  },
+  {
     "sainnhe/sonokai",
     lazy = false,
     priority = 1000,
@@ -67,8 +82,68 @@ return {
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
+      vim.g.sonokai_style = "maia"
       vim.g.sonokai_enable_italic = true
+      vim.g.sonokai_palette_override = {
+        red = { "#c97b84", "203" },
+        green = { "#7aab6e", "107" },
+      }
       vim.cmd.colorscheme("sonokai")
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    enabled = false,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        integrations = {
+          telescope = { enabled = true },
+          nvim_tree = true,
+          gitsigns = true,
+          which_key = true,
+          notify = true,
+          blink_cmp = true,
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    enabled = false,
+    config = function()
+      require("kanagawa").setup({
+        compile = false,
+        undercurl = true,
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = true,
+        dimInactive = false,
+        terminalColors = true,
+        colors = {
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(_)
+          return {}
+        end,
+        theme = "wave",
+        background = {
+          dark = "wave",
+          light = "lotus",
+        },
+      })
+      vim.cmd("colorscheme kanagawa")
     end,
   },
 }
